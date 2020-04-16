58bac2add58185b5d08bfb436a2974242835aff0
yangseongyeal2 <61301184+yangseongyeal@users.noreply.github.com>
2020-04-16 17:37:36 +0900

    finishSliding

--- app/src/main/java/com/example/kupleapp/MainActivity.java
+++ app/src/main/java/com/example/kupleapp/MainActivity.java
@@ -4,16 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.example.kupleapp.Model.SliderItem;
+import com.google.android.material.tabs.TabLayout;
+import com.smarteist.autoimageslider.IndicatorAnimations;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
     ImageButton go;
-    Adapter adapter;
-    ViewPager viewPager;
+    SliderView sliderView;
+    private SliderAdapterExample adapter;
 
 
     @Override
@@ -31,9 +41,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        viewPager=(ViewPager)findViewById(R.id.view);
-        adapter=new Adapter(this);
-        viewPager.setAdapter(adapter);
+        sliderView = findViewById(R.id.imageSlider);
+
+        adapter = new SliderAdapterExample(this);
+        sliderView.setSliderAdapter(adapter);
+
+        sliderView.setIndicatorAnimation(IndicatorAnimations.THIN_WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_RIGHT);
+        sliderView.setIndicatorSelectedColor(Color.WHITE);
+        sliderView.setIndicatorUnselectedColor(Color.GRAY);
+        sliderView.setScrollTimeInSec(3);
+        sliderView.setAutoCycle(false);
+        int[] img={R.drawable.one,R.drawable.two,R.drawable.three};
+       // List<SliderItem> sliderItemList = new ArrayList<>();
+
+        for(int i=0;i<3;i++){
+            SliderItem sliderItem = new SliderItem();
+            sliderItem.setDescription("Slider Item"+i);
+            sliderItem.setImageUrl(img[i]);
+           // sliderItemList.add(sliderItem);
+            adapter.addItem(sliderItem);
+        }
+
 
 
     }
--- app/src/main/java/com/example/kupleapp/SecondActivity.java
+++ app/src/main/java/com/example/kupleapp/SecondActivity.java
@@ -10,5 +10,4 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-    }
-}
+    }}
